8dae358423c380a4abd825ab80ae5f5620839007
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-06-23 14:04:00 +0100

    Task 1: first commit & setup spring boot applcation

---
+++ server-status-tracker/src/test/java/com/server/status/tacker/ServerStatusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.status.tacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStatusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
