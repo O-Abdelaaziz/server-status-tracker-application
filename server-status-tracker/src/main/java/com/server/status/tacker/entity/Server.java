3d6d2c2f1abaf99c998c0bd4b319ceee33bee2ff
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-06-24 08:33:55 +0100

    Task 2: setup datasource & create entities

---
+++ server-status-tracker/src/main/java/com/server/status/tacker/entity/Server.java
@@ -0,0 +1,50 @@
+package com.server.status.tacker.entity;
+
+import com.server.status.tacker.enumeration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @Created 23/06/2024 - 14:06
+ * @Package com.server.status.tacker.entity
+ * @Project server-status-tracker
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "servers")
+public class Server implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    @Column(name = "ip_address",unique = true)
+    private String ipAddress;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "memory")
+    private String memory;
+
+    @Column(name = "icon")
+    private String icon;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "status")
+    private Status status;
+}
---
+++ server-status-tracker/src/main/java/com/server/status/tacker/enumeration/Status.java
@@ -0,0 +1,23 @@
+package com.server.status.tacker.enumeration;
+
+/**
+ * @Created 23/06/2024 - 14:10
+ * @Package com.server.status.tacker.enumeration
+ * @Project server-status-tracker
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+
+    private String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    private String getStatus(){
+        return
+    }
+}
